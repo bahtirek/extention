2b036ada0e91a9fb7d5a663936a62a534ee0d7b9
bahti <bahtirek@gmail.com>
2021-10-26 18:29:40 -0400

    first commit

---
+++ extension-move.js
@@ -0,0 +1,33 @@
+
+
+var extension = document.getElementById('extension');
+var containers = document.getElementsByClassName('container');
+var viewportOffset = extension.getBoundingClientRect();
+document.even
+
+extension.addEventListener('mousedown',function(event){
+    event.stopPropagation();
+    event.preventDefault();
+    if(event.target === extension) {
+        onMouseDown();
+    }
+},true);
+
+extension.addEventListener('mouseup',function(event){
+    event.stopPropagation();
+    event.preventDefault();
+    onMouseUp();
+},true);
+
+extension.addEventListener('mouseout',function(event){
+    event.stopPropagation();
+    event.preventDefault();
+    onMouseUp();
+},true);
+
+function moveExtension(e){
+    var posX = e.pageX;
+    var posY = e.pageY;
+    console.log(posX, posY);
+}
+
---
+++ mousedown.js
@@ -0,0 +1,75 @@
+// This timeout, started on mousedown, triggers the beginning of a hold
+
+var holdStarter = null;
+
+ 
+
+// This is how many milliseconds to wait before recognizing a hold
+
+var holdDelay = 200;
+
+ 
+
+// This flag indicates the user is currently holding the mouse down
+
+var holdActive = false;
+
+ 
+
+// MouseDown
+
+ 
+
+function onMouseDown(){
+
+
+    // Do not take any immediate action - just set the holdStarter
+
+    //  to wait for the predetermined delay, and then begin a hold
+
+    holdStarter = setTimeout(function() {
+
+        holdStarter = null;
+
+        holdActive = true;
+
+        // begin hold-only operation here, if desired
+        // read mouse position
+        console.log('down');
+        window.addEventListener('mousemove', moveExtension, true);
+
+    }, holdDelay);
+
+}
+
+ 
+
+// MouseUp
+
+function onMouseUp(){
+
+    // If the mouse is released immediately (i.e., a click), before the
+
+    //  holdStarter runs, then cancel the holdStarter and do the click
+
+    if (holdStarter) {
+
+        clearTimeout(holdStarter);
+
+        // run click-only operation here 
+        window.removeEventListener('mousemove', moveExtension, true);
+
+    }
+
+    // Otherwise, if the mouse was being held, end the hold
+
+    else if (holdActive) {
+
+        holdActive = false;
+
+        // end hold-only operation here, if desired
+        window.removeEventListener('mousemove', moveExtension, true);
+
+    }
+
+}
