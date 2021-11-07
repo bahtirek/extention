abe82619eafcfd9ae2c81275af37deb3e3b70818
bahti <bahtirek@gmail.com>
2021-11-07 01:04:10 -0400

    extrention toggle

--- js/background/content.js
+++ js/background/content.js
@@ -4,11 +4,22 @@ chrome.runtime.onMessage.addListener(gotMessage);
 
 function gotMessage(message, sender, sendResponse) {
     if (message === 'inject') {
-        fetch(chrome.runtime.getURL('/extention.html')).then(r => r.text()).then(html => {
-            document.body.insertAdjacentHTML('beforeend', html);
-            setButtonListeners();
-            moveExtention();
-            resizeExtention();
-        });
+        let extention = document.getElementById('ui-br-ext-extention');
+        if(extention == null) {
+            fetch(chrome.runtime.getURL('/extention.html')).then(r => r.text()).then(html => {
+                document.body.insertAdjacentHTML('beforeend', html);
+                setButtonListeners();
+                moveExtention();
+                resizeExtention();
+            });
+        } else {
+            if (extention.style.display == 'block') {
+                extention.style.display = 'none';
+            } else {
+                extention.style.display = 'block';
+            }
+        }
+
+        
     }
 }
