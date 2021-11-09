617a004c0286f158fa8ea1fddac06c319027096c
bahti <bahtirek@gmail.com>
2021-11-08 21:59:26 -0500

    toggle ext

--- js/background/content.js
+++ js/background/content.js
@@ -5,6 +5,9 @@ chrome.runtime.onMessage.addListener(gotMessage);
 function gotMessage(message, sender, sendResponse) {
     if (message === 'inject') {
         let extention = document.getElementById('ui-br-ext-extention');
+        
+        // check if extention is injected
+        // if not, inject extention
         if(extention == null) {
             fetch(chrome.runtime.getURL('/extention.html')).then(r => r.text()).then(html => {
                 document.body.insertAdjacentHTML('beforeend', html);
@@ -13,13 +16,12 @@ function gotMessage(message, sender, sendResponse) {
                 resizeExtention();
             });
         } else {
+            //if extension injected then check and toggle visibility
             if (extention.style.display == 'block') {
                 extention.style.display = 'none';
             } else {
                 extention.style.display = 'block';
             }
         }
-
-        
     }
 }
