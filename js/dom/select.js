c963c94f150f9f19da327026f14c7abc6a96af2f
asultanov88 <uzsultanov@gmail.com>
2021-11-08 21:58:09 -0500

    commited select changes.

--- js/dom/select.js
+++ js/dom/select.js
@@ -10,6 +10,12 @@ function onSelect(){
         -ms-user-select: none;
         user-select: none;
       }`;
+
+    const outlineSelectedElement = `.ui-br-ext-outlined-element{ 
+        outline: 3px dashed!important;
+        outline-color: red!important; 
+      }`
+
     const head = document.head || document.getElementsByTagName('head')[0];
 
     const style = document.createElement('style');
@@ -22,7 +28,7 @@ function onSelect(){
     if (style.styleSheet){
         style.styleSheet.cssText = noPointerEvent+noHighlight;
     } else {
-        style.appendChild(document.createTextNode(noPointerEvent+noHighlight));
+        style.appendChild(document.createTextNode(noPointerEvent+noHighlight+outlineSelectedElement));
     }
 
     addClickToBody(getMouseCoordinates);
@@ -33,12 +39,17 @@ function onDeselect(){
     document.getElementById('ui-br-ext-extention-style')?.remove();
 
     removeClickFromBody(getMouseCoordinates);
+    
+    // Remove outline from any previously selected elements.
+    document.querySelectorAll('.ui-br-ext-outlined-element').forEach(element => {
+        element.classList.remove('ui-br-ext-outlined-element');
+    });
 
 }
 
 function addClickToBody(eventFunction){
 
-    const body = document.getElementsByTagName('body')[0];
+    const body = document.getElementsByTagName('html')[0];
 
     body.addEventListener('mousedown', eventFunction, true);
 
@@ -46,7 +57,7 @@ function addClickToBody(eventFunction){
 
 function removeClickFromBody(eventFunction){
 
-    const body = document.getElementsByTagName('body')[0];
+    const body = document.getElementsByTagName('html')[0];
 
     body.removeEventListener('mousedown', eventFunction, true);
 
@@ -55,9 +66,9 @@ function removeClickFromBody(eventFunction){
 function getMouseCoordinates(event){
 
     if(event){
-        const pageX = event.pageX;
+        const pageX = event.clientX;
 
-        const pageY = event.pageY;
+        const pageY = event.clientY;
 
         findElementFromPoint(pageX, pageY);
 
@@ -73,13 +84,30 @@ function findElementFromPoint(pageX, pageY){
     const element = document.elementFromPoint(pageX, pageY);
 
     if(
-        element.tagName.toLocaleLowerCase() != 'body'
-        && element.closest('#ui-br-ext-extention') === null
+        element?.tagName.toLocaleLowerCase() != 'body'
+        && element?.closest('#ui-br-ext-extention') === null
         ){
-        console.log(element);
+            
+            // Enabling the 'pointer-evenet: none' after locating the element under the pointer.
+            onSelect();
+            outlineSelectedElement(element);
+    }else{
+      // Enabling the 'pointer-evenet: none' after locating the element under the pointer.
+      onSelect();          
     }
+}
 
-    // Enabling the 'pointer-evenet: none' after locating the element under the pointer.
-    onSelect();
+/**
+ * It styles the selected element by outlining it.
+ * @param {selected element} element 
+ */
+function outlineSelectedElement(element){
+
+    // Remove outline from any previously selected elements.
+    document.querySelectorAll('.ui-br-ext-outlined-element').forEach(element => {
+        element.classList.remove('ui-br-ext-outlined-element');
+    });
+
+    element.classList.add('ui-br-ext-outlined-element');
 
 }
