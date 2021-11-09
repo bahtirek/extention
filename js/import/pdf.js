5fe66c948e9ef59bbc6d980cea6f215635c37d3a
bahti <bahtirek@gmail.com>
2021-11-08 22:00:32 -0500

    dom inspection alert

---
+++ js/import/pdf.js
@@ -0,0 +1,23 @@
+var dd = {
+	content: [
+		{
+			text: 'Subheader 1 - using subheader style',
+			style: 'subheader'
+		},
+		'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Confectum ponit legam, perferendis nomine miserum, animi. Moveat nesciunt triari naturam posset, eveniunt specie deorsus efficiat sermone instituendarum fuisse veniat, eademque mutat debeo. Delectet plerique protervi diogenem dixerit logikh levius probabo adipiscuntur afficitur, factis magistra inprobitatem aliquo andriam obiecta, religionis, imitarentur studiis quam, clamat intereant vulgo admonitionem operis iudex stabilitas vacillare scriptum nixam, reperiri inveniri maestitiam istius eaque dissentias idcirco gravis, refert suscipiet recte sapiens oportet ipsam terentianus, perpauca sedatio aliena video.\n\n',
+		`test
+		test
+		test`,
+		{
+			text: 'Subheader 2 - using subheader style',
+			style: 'subheader'
+		},
+		'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Confectum ponit legam, perferendis nomine miserum, animi. Moveat nesciunt triari naturam posset, eveniunt specie deorsus efficiat sermone instituendarum fuisse veniat, eademque mutat debeo. Delectet plerique protervi diogenem dixerit logikh levius probabo adipiscuntur afficitur, factis magistra inprobitatem aliquo andriam obiecta, religionis, imitarentur studiis quam, clamat intereant vulgo admonitionem operis iudex stabilitas vacillare scriptum nixam, reperiri inveniri maestitiam istius eaque dissentias idcirco gravis, refert suscipiet recte sapiens oportet ipsam terentianus, perpauca sedatio aliena video.\n\n',
+	],
+	styles: {
+		subheader: {
+			fontSize: 15,
+			bold: true
+		}
+	}
+} 
