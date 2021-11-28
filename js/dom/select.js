398fc43689e26701c12d9a0ca9a5ee13267e7a95
bahti <bahtirek@gmail.com>
2021-11-28 11:10:10 -0500

    moved styles back to js

--- js/dom/select.js
+++ js/dom/select.js
@@ -1,5 +1,31 @@
 function onSelect(){
     console.log('on select');
+    const noPointerEvent = 'body *{pointer-events: all!important; }';
+    const noHighlight = `*{
+        -webkit-tap-highlight-color: transparent;
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }`;
+
+
+    const head = document.head || document.getElementsByTagName('head')[0];
+
+    const style = document.createElement('style');
+
+    style.setAttribute('id','ui-br-ext-extention-style');    
+
+    head.appendChild(style);
+
+    style.type = 'text/css';
+    if (style.styleSheet){
+        style.styleSheet.cssText = noPointerEvent+noHighlight;
+    } else {
+        style.appendChild(document.createTextNode(noPointerEvent+noHighlight));
+    }
     addClickToBody(getMouseCoordinates);
 }
 
